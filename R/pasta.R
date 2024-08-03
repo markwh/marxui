48a6fdbad192b6401f56bfc278c29569b5eee09c
Mark Hagemann <mark.hagemann@gmail.com>
2024-08-03 12:17:10 -0400

    Add pasta addin

--- R/pasta.R
+++ R/pasta.R
@@ -12,5 +12,21 @@ pasta <- function(filename, dir = Sys.getenv("PASTA_DIR"), edit = TRUE) {
   if (!dir.exists(root_dir)) {
     stop("PASTA_DIR environment variable must be set to an existing directory.")
   }
-  rmarkdown::draft(filename, template = "pasta-default", package = "marxui", edit = edit)
+  
+  rmarkdown::draft(
+    file = file.path(root_dir, filename), 
+    template = "pasta-default", 
+    package = "marxui", 
+    edit = edit
+  )
+}
+
+#' Call the addin--just a wrapper for `pasta()` with a timestamped filename
+#' 
+#' @export
+call_pasta_addin <- function() {
+  
+  filename <- paste0("pasta", format(Sys.time(), "%Y%m%d_%H%M%S"))
+  
+  pasta(filename)
 }
